1cffee505d8a81ed63906d91827e8dd12da329e1
Nrapendra Trivedi <trivajay259@gmail.com>
2024-10-24 16:20:54 +0200

    updated EnvironmentVariableListener

--- sf-account-application/sf-account-service/src/main/java/com/nrapendra/EnvironmentVariableListener.java
+++ sf-account-application/sf-account-service/src/main/java/com/nrapendra/EnvironmentVariableListener.java
@@ -6,6 +6,8 @@ import org.springframework.context.ApplicationListener;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Properties;
 
 @Slf4j
@@ -14,13 +16,18 @@ public class EnvironmentVariableListener implements ApplicationListener<Applicat
     @Override
     public void onApplicationEvent(ApplicationEnvironmentPreparedEvent event) {
         try {
-            Properties props = new Properties();
-            FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "\\" + ".env");
-            props.load(fis);
+            var props = new Properties();
+            var path =  System.getProperty("user.dir") + "/.env";
+            var pathValue = Paths.get(path);
+            var isFileExist = Files.exists(pathValue);
+            if(isFileExist) {
+                var fis = new FileInputStream(path);
+                props.load(fis);
 
-            props.forEach((key, value) -> {
-                System.setProperty(key.toString(), value.toString());
-            });
+                props.forEach((key, value) -> {
+                    System.setProperty(key.toString(), value.toString());
+                });
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
