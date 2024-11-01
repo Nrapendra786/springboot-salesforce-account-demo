fa1b255f4d75f4b92acfb551c870bcc46ba675da
Nrapendra Trivedi <trivajay259@gmail.com>
2024-11-01 02:21:13 +0100

    updated docker files and updated configuration as per need

---
+++ sf-account-application/commons/src/main/java/com/nrapendra/Common.java
@@ -0,0 +1,9 @@
+package com.nrapendra;
+
+public class Common {
+
+    public static void main(String [] args){
+      // main class
+    }
+
+}
--- sf-account-application/sf-account-management-service/src/test/java/com/nrapendra/consumer/KafkaConsumerServiceTest.java
+++ sf-account-application/sf-account-management-service/src/test/java/com/nrapendra/consumer/KafkaConsumerServiceTest.java
@@ -1,9 +1,7 @@
 package com.nrapendra.consumer;
 
 import com.nrapendra.consumer.events.EventRepository;
-import lombok.extern.slf4j.Slf4j;
 import org.junit.ClassRule;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,23 +13,15 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.testcontainers.containers.ContainerLaunchException;
-import org.testcontainers.kafka.KafkaContainer;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.utility.DockerImageName;
-
-import static com.nrapendra.utils.AppUtil.TOPIC_NAME;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = ConsumerApplication.class,
-        webEnvironment = SpringBootTest.WebEnvironment.NONE,
-        properties = {
-                "spring.kafka.consumer.enable-auto-commit=false"
-        }
-)
+@SpringBootTest(classes = ConsumerApplication.class, webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @ActiveProfiles("test")
-@ContextConfiguration(initializers = {KafkaConsumerServiceTest.Initializer.class})
-@Slf4j
+@ContextConfiguration(initializers = { KafkaConsumerServiceTest.Initializer.class })
 public class KafkaConsumerServiceTest {
 
     @Autowired
@@ -41,20 +31,8 @@ public class KafkaConsumerServiceTest {
     public KafkaTemplate<String, String> kafkaTemplate;
 
     @ClassRule
-    public static KafkaContainer kafkaContainer =
-            new KafkaContainer(DockerImageName.parse("apache/kafka-native:3.8.0"))
-                    .withEnv("KAFKA_AUTO_CREATE_TOPICS_ENABLE", "true")
-                    .withEnv("KAFKA_CREATE_TOPICS", TOPIC_NAME); // Ensure TOPIC_NAME is defined
-
-    @BeforeAll
-    public static void before() {
-        try {
-            kafkaContainer.start();
-        } catch (ContainerLaunchException e) {
-            log.error("Container logs: {}", kafkaContainer.getLogs());
-            throw e; // Rethrow the exception after logging
-        }
-    }
+    public static KafkaContainer kafkaContainer = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:latest"))
+    .waitingFor(Wait.forListeningPort());
 
     static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
         public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
@@ -65,12 +43,12 @@ public class KafkaConsumerServiceTest {
 
     @Test
     public void messageAreReceived() throws Exception {
-        kafkaTemplate.send(TOPIC_NAME, messages());
+        kafkaTemplate.send("my_topic", messages());
         assertTrue(eventRepository.count() > 0);
     }
 
     private String messages() {
-        return "{\"id\":\"test_id_1\",\"schema_id\":\"schema_id_test_1\",\"payload\":\"dGVzdF8x\"}" +
+        return  "{\"id\":\"test_id_1\",\"schema_id\":\"schema_id_test_1\",\"payload\":\"dGVzdF8x\"}" +
                 "{\"id\":\"test_id_2\",\"schema_id\":\"schema_id_test_2\",\"payload\":\"dGVzdF8y\"}" +
                 "{\"id\":\"test_id_3\",\"schema_id\":\"schema_id_test_3\",\"payload\":\"dGVzdF8z\"}" +
                 "{\"id\":\"test_id_4\",\"schema_id\":\"schema_id_test_4\",\"payload\":\"dGVzdF80\"}" +
